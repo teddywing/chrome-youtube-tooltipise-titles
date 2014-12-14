6c34918ec98aedcbfd0778584ed25401435194bb
Teddy Wing <git@teddywing.com>
2014-12-14 14:24:30 -0500

    tooltipise.js: Handle AJAX load of more videos
    New videos loaded on a page over AJAX will not have title tooltips.
    Refresh tooltip titles when more videos are loaded.
    
    Just use a timeout to make things simple. The tooltipiser will wait 3
    seconds between requesting more videos and refreshing titles. This will
    generally provide enough time to load the videos into the DOM unless the
    network connection is really slow.

--- tooltipise.js
+++ tooltipise.js
@@ -1,7 +1,17 @@
 (function() {
-	var title_elements = document.querySelectorAll('.yt-uix-sessionlink.yt-uix-tile-link');
-	
-	for (var i = 0; i < title_elements.length; i++) {
-		title_elements[i].setAttribute('title', title_elements[i].innerText);
+	function tooltipise_titles () {
+		var title_elements = document.querySelectorAll('.yt-uix-sessionlink.yt-uix-tile-link');
+		
+		for (var i = 0; i < title_elements.length; i++) {
+			title_elements[i].setAttribute('title', title_elements[i].innerText);
+		}
 	}
+	
+	// Initial load
+	tooltipise_titles();
+	
+	// Refresh titles when more videos are loaded
+	document.querySelector('.load-more-text').addEventListener('click', function() {
+		window.setTimeout(tooltipise_titles, 3000);
+	});
 })()
